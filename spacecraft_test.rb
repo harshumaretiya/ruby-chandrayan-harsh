8f931aa0f55ada6afb930671911d7d1c7e8eca17
harshumaretiya <harsh.patel.hp846@gmail.com>
2024-02-11 11:30:30 +0530

    test: modified and optimised test cases

--- spacecraft_test.rb
+++ spacecraft_test.rb
@@ -1,51 +1,40 @@
-require_relative '../spacecraft'
+require_relative 'Spacecraft'
 
-RSpec.describe Spacecraft do
-  describe 'initialization' do
-    subject(:spacecraft) { Spacecraft.new(0, 0, 0, 'N') }
-
-    it 'initializes with x, y, z coordinates and direction' do
-      expect(spacecraft.x).to eq(0)
-      expect(spacecraft.y).to eq(0)
-      expect(spacecraft.z).to eq(0)
-      expect(spacecraft.direction).to eq('N')
-    end
+class SpacecraftTest
+  def self.run_all_tests
+    puts "Running tests..."
+    new.test_forward_and_turn
+    new.test_up_and_down
+    new.test_backward
+    puts "All tests completed."
   end
 
-  # Additional tests for movement and turning
-  describe 'movement' do
-    it 'moves forward' do
-      spacecraft = Spacecraft.new(0, 0, 0, 'N')
-      spacecraft.execute_commands(['f'])
-      expect(spacecraft.y).to eq(1)
-    end
-
-    it 'turns right' do
-      spacecraft = Spacecraft.new(0, 0, 0, 'N')
-      spacecraft.execute_commands(['r'])
-      expect(spacecraft.direction).to eq('E')
-    end
-
-    it 'turns left' do
-      spacecraft = Spacecraft.new(0, 0, 0, 'N')
-      spacecraft.execute_commands(['l'])
-      expect(spacecraft.direction).to eq('W')
-    end
+  def test_forward_and_turn
+    spacecraft = Spacecraft.new(0, 0, 0, 'N')
+    spacecraft.execute_commands(%w[f r f l f])
+    assert_equal([2, 1, 0], [spacecraft.x, spacecraft.y, spacecraft.z], "Test Forward and Turn")
+    assert_equal('N', spacecraft.direction, "Test Forward and Turn")
   end
 
-  describe 'elevation change' do
-    it 'moves up' do
-      spacecraft = Spacecraft.new(0, 0, 0, 'N')
-      spacecraft.execute_commands(['u'])
-      expect(spacecraft.z).to eq(1)
-    end
-  
-    it 'moves down' do
-      spacecraft = Spacecraft.new(0, 0, 1, 'N')
-      spacecraft.execute_commands(['d'])
-      expect(spacecraft.z).to eq(0)
+  def test_up_and_down
+    spacecraft = Spacecraft.new(0, 0, 0, 'N')
+    spacecraft.execute_commands(%w[u r d l])
+    assert_equal([0, 0, 0], [spacecraft.x, spacecraft.y, spacecraft.z], "Test Up and Down")
+    assert_equal('W', spacecraft.direction, "Test Up and Down")
+  end
+
+  def test_backward
+    spacecraft = Spacecraft.new(0, 0, 0, 'E')
+    spacecraft.execute_commands(%w[b l b r b])
+    assert_equal([-1, -1, 0], [spacecraft.x, spacecraft.y, spacecraft.z], "Test Backward")
+    assert_equal('W', spacecraft.direction, "Test Backward")
+  end
+
+  def assert_equal(expected, actual, test_name)
+    if expected == actual
+      puts "#{test_name}: Passed"
+    else
+      puts "#{test_name}: Failed (Expected #{expected}, but got #{actual})"
     end
   end
 end
-
-
